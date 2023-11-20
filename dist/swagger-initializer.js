38f8ff15db22c4f4f64651528241a313a40567dc
Giacomo Ronconi <36189778+giacomoronconiobda@users.noreply.github.com>
2023-11-20 19:04:07 +0100

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,15 +1,9 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
-  var vars = [], hash;
-  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-  for (var i = 0; i < hashes.length; i++) {
-    hash = hashes[i].split('=');
-    vars.push(hash[0]);
-    vars[hash[0]] = decodeURIComponent(hash[1]);
-  }
+  var api = window.location.pathname.substr(10)
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "apis/" + vars.api,
+    url: "apis/" + api + ".yaml,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
