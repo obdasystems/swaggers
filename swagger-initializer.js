5847dd1c84b98a6cac03a5fb735fccef4e5e50e7
Giacomo Ronconi <36189778+giacomoronconiobda@users.noreply.github.com>
2023-11-20 19:55:37 +0100

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
   var api = window.location.pathname.substr(15)
-  api = api.slie(0, -1)
+  api = api.slice(0, -1)
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: api + ".yaml",
