7288e3517ebb2c5e0dff0d8dd7db0c97ab926a71
Giacomo Ronconi <36189778+giacomoronconiobda@users.noreply.github.com>
2023-11-20 18:38:34 +0100

    Select api from URL query param

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,9 +1,15 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
-
+  var vars = [], hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for (var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = decodeURIComponent(hash[1]);
+  }
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "apis/rdf-graph.yaml",
+    url: "apis/" + vars.api,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
