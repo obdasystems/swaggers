a7a2997725e86147831b37c514ec3488f804de44
giacomoronconiobda <ronconi@diag.uniroma1.it>
2025-05-08 16:57:33 +0200

    🎨 New repo structurefor gh pages

--- ui/swagger-initializer.js
+++ ui/swagger-initializer.js
@@ -1,10 +1,10 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
-  var api = window.location.pathname.substr(15)
+  var api = window.location.search.substring(1).split('=')[1]
   api = api.slice(0, -1)
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: api + ".yaml",
+    url: "../bundled/" + api + ".yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
