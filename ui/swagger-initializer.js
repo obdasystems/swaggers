c00cd7898ba7c925b70d885f958ca58f6681a620
Giacomo Ronconi <ronconi@diag.uniroma1.it>
2025-05-09 15:01:43 +0200

    🐛 Fix api name

--- ui/swagger-initializer.js
+++ ui/swagger-initializer.js
@@ -1,7 +1,6 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
   var api = window.location.search.substring(1).split('=')[1]
-  api = api.slice(0, -1)
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "../bundled/" + api + ".yaml",
