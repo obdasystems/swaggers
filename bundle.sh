6cb03678dce455288e7a6518ac1c1428321a5fd8
Giacomo Ronconi <ronconi@diag.uniroma1.it>
2025-05-14 12:44:43 +0200

    📝 Add designer ai bundle

--- bundle.sh
+++ bundle.sh
@@ -1,2 +1,4 @@
 npx @redocly/cli@latest bundle
 npx @redocly/cli@latest join -o bundled/mastro.yaml --prefix-tags-with-filename
+
+npx @redocly/cli@latest bundle apis/ontology-designer-ai.yaml -o bundled/ontology-designer-ai.yaml 
