5fec1b2ce3f858a3425705a6022b9e611880daa0
Gianluca Pepe <37369540+gianluca-pepe@users.noreply.github.com>
2025-05-30 12:38:16 +0200

    ♻️ rename ai module

--- bundle.sh
+++ bundle.sh
@@ -1,4 +1,4 @@
 npx @redocly/cli@latest bundle
 npx @redocly/cli@latest join -o bundled/mastro.yaml --prefix-tags-with-filename
 
-npx @redocly/cli@latest bundle apis/ontology-designer-ai.yaml -o bundled/ontology-designer-ai.yaml 
+npx @redocly/cli@latest bundle apis/ai-assistant.yaml -o bundled/ai-assistant.yaml 
