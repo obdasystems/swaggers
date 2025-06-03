1decd490d803ec8a73ec983623749979ac2b7d87
Giacomo Ronconi <ronconi@diag.uniroma1.it>
2025-06-03 12:10:45 +0200

    👷 Add monolith bundle (mastro + ai assistant)

--- bundle.sh
+++ bundle.sh
@@ -2,3 +2,4 @@ npx @redocly/cli@latest bundle
 npx @redocly/cli@latest join -o bundled/mastro.yaml --prefix-tags-with-filename
 
 npx @redocly/cli@latest bundle apis/ai-assistant.yaml -o bundled/ai-assistant.yaml 
+npx @redocly/cli@latest join bundled/mastro.yaml bundled/ai-assistant.yaml  -o bundled/monolith.yaml --prefix-tags-with-filename
